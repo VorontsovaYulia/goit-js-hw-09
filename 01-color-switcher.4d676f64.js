a1d91c82f1bfff49aeeeec2caa44aadc23a78825
VorontsovaYulia <123415423+VorontsovaYulia@users.noreply.github.com>
2023-06-26 17:55:31 +0000

    Deploying to gh-pages from @ VorontsovaYulia/goit-js-hw-09@1439bed1f0f42d6e421deb5d813dec341e87b063 🚀

--- 01-color-switcher.4d676f64.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1;var a=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),a}),1e3)})),e.addEventListener("click",(function(){clearInterval(id),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.4d676f64.js.map
