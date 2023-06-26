e168ee210bed0b611707848b763641037ee48ea0
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-06-26 20:39:26 +0300

    added 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector(".form");
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (shouldResolve) {
+    
+    resolve({ position, delay })
   } else {
-    // Reject
+
+    reject({ position, delay })
   }
+    }, delay)
+  })
+  promise
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    })
+  .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+    })
+  
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+ 
+
+  const current = evt.currentTarget.elements;
+  
+  let inputDelay = Number(current.delay.value);
+  const inputStep = Number(current.step.value);
+  const inputAmount = Number(current.amount.value);
+  
+
+  for (let i = 0; i < inputAmount; i += 1){
+    if (i > 0) {
+      inputDelay += inputStep;
+    }
+    createPromise(i+1, inputDelay)
+  }
+  form.reset();
 }
